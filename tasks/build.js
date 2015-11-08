7cb04646b9127db41c507182ef90f36da2a6a0b6
Drawain <drawain@gmail.com>
2015-11-08 19:29:49 +0100

    chore(packages): inline requires in the build tasks

--- tasks/build.js
+++ tasks/build.js
@@ -1,26 +1,24 @@
 'use strict';
 
-var del = require('del');
-var prompt = require('gulp-prompt');
-var exec = require('gulp-exec');
-
 module.exports = function (gulp, config) {
 
   return {
 
     clean: function (cb) {
-      del([config.build.distPath + '**/*'], cb);
+      require('del')([config.build.distPath + '**/*'], cb);
     },
 
     deploy: function() {
-      var reportOptions = { err: true, stderr: true, stdout: true };
+      var prompt = require('gulp-prompt');
+      var exec = require('gulp-exec');
+
       return gulp.src('gulpfile.js')
         .pipe(prompt.confirm({
           message: 'Do you really want to deploy to production?',
           default: false
         }))
         .pipe(exec('git pull origin master; git push origin master;git push origin master:production'))
-        .pipe(exec.reporter(reportOptions));
+        .pipe(exec.reporter({ err: true, stderr: true, stdout: true }));
     }
 
   };
