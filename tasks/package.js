b8ec9788f395f695d69ac2ea111a4d4e1ac9df5e
Drawain <drawain@gmail.com>
2015-11-08 19:32:38 +0100

    chore(packages): inline requires in the packages tasks

--- tasks/package.js
+++ tasks/package.js
@@ -1,16 +1,14 @@
 'use strict';
 
-var install = require('gulp-install');
-
 module.exports = function(gulp, config) {
   return {
     watch: function() {
-      gulp.watch(config.package.path+'/package.json').on('change', this.install);
+      gulp.watch(config.package.path + '/package.json').on('change', this.install);
     },
 
     install: function() {
       console.log('Installing NPM packages');
-      install();
+      require('gulp-install')();
     }
   };
 };
